053beda2fb89514a82da5127b25339de812b8fcf
Alex Barashkov <lnikell@gmail.com>
2019-12-25 15:02:51 +0100

    Fixes to make npx works correctly

---
+++ src/importer.js
@@ -0,0 +1,40 @@
+const { join } = require('path');
+const getDirectories = require('./helpers/getDirectories');
+
+const snippetsPath = join(process.cwd(), 'snippets');
+const WHITELISTED_LANGUAGES = ['javascript'];
+
+const { Snippet } = require('./models');
+
+const importSnippets = () => {
+  const languages = getDirectories(snippetsPath);
+  languages.forEach((language) => {
+    if (!WHITELISTED_LANGUAGES.includes(language)) { return; }
+    const packages = getDirectories(join(snippetsPath, language));
+    packages.forEach(async (name) => {
+      // eslint-disable-next-line global-require, import/no-dynamic-require
+      const snippetsData = require(join(snippetsPath, language, name, `${name}.json`));
+      const snippetsDefaults = {
+        language,
+        name,
+        snippets: snippetsData,
+      };
+      // TODO add snippet validation logic
+
+      const [snippet, created] = await Snippet.findOrCreate({
+        where: {
+          language,
+          name,
+        },
+        defaults: {
+          ...snippetsDefaults,
+        },
+      });
+      if (!created && snippet) {
+        await snippet.update({ snippets: snippetsData });
+      }
+    });
+  });
+};
+
+module.exports = importSnippets;
